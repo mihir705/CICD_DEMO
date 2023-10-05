6cc069b05fe20f3eb1a70386ddbd4686372915e1
mihir popat <mihirpopat705@gmail.com>
2023-10-05 10:40:14 +0530

    Initial Commit

---
+++ CICD_DEMO/CICD_DEMOApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICD_DEMOApp.swift
+//  CICD_DEMO
+//
+//  Created by MIHIR ANILKUMAR POPAT on 05/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CICD_DEMOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
